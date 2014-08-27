83410606248e9b4821ed14aa07196dfe295b5d6d
kgryte <kgryte@gmail.com>
2014-08-27 01:28:30 -0700

    [UPDATE] added example code.

---
+++ examples/index.js
@@ -0,0 +1,15 @@
+
+
+// MODULES //
+
+var createFigure = require( './../lib' );
+
+
+// Data:
+
+var data = require( './line.data.json' );
+
+
+// FIGURE //
+
+console.log( createFigure( {}, data ) );
